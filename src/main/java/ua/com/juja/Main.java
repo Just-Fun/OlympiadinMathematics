7a506e805e0cb64cdad0640cdab168ba7f11aec0
Just-Fun <Just-Fun@users.noreply.github.com>
2016-08-12 15:37:37 +0300

    Begining 12.08.2016

---
+++ src/main/java/ua/com/juja/Main.java
@@ -0,0 +1,10 @@
+package ua.com.juja;
+
+/**
+ * Created by Serzh on 8/12/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/ua/com/juja/R.java
@@ -0,0 +1,7 @@
+package ua.com.juja;
+
+/**
+ * Created by Serzh on 8/12/16.
+ */
+public class R {
+}
